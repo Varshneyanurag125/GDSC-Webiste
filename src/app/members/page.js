f61d26a0104dbcf9c6ea153e11495862ad5d0e54
Varshneyanurag125 <22bee031@nith.ac.in>
2024-03-02 17:43:35 +0530

    initial commit

---
+++ src/app/members/page.js
@@ -0,0 +1,20 @@
+export default function Members(){
+    return(
+        <div className=" flex flex-col gap-2 pt-1">
+            <div className=" w-full h-20 border border-red-500">
+
+            </div>
+            <div className=" w-full h-80  border border-yellow-400 flex flex-col items-center justify-center gap-4 " >
+                <div className="border border-white"><h1 className=" text-white text-8xl leading-tight">ANURAG VARSHNEY</h1></div>
+                <div className=" border border-white text-lg flex items-center justify-between text-white gap-10 p-2">
+                    <div><h2>Web Developer</h2></div>
+                    <div><h2>UI/UX</h2></div>
+                    <div><h2>MY Work 1</h2></div>
+                    <div><h2>My work 1</h2></div>
+                    <div><h2>My work 1</h2></div>
+                </div>
+            </div>
+            <div className=" w-full h-80 border border-red-500"></div>
+        </div>
+    )
+}
---
+++ src/components/Members.js
@@ -0,0 +1,9 @@
+
+
+// export default function Members(){
+//     return(
+//         <div>
+            
+//         </div>
+//     )
+// }
